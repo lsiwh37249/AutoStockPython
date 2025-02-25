722e948d22d2bed4846487f2d5103778851ac7a3
kim <lsiwh32749@gmail.com>
2025-02-25 14:48:51 +0900

    add : Trader 추상 클래스 메소드 추가

---
+++ src/autostockpython/module/Trader.py
@@ -0,0 +1,14 @@
+#import ABC
+
+class Trader():
+    def __init__():
+        
+
+    def initialize():
+        
+
+    def send_request():
+        pass
+
+    def get_account_info():
+        pass
